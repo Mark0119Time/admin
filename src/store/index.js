3b3c264a2ac5aaaac6ce87f7c9101248f6ebcb4b
Mark0119 <2528814791@qq.com>
2020-11-23 23:10:04 +0800

    空白模板

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  // 数据存储
+  state: {},
+  // 数据更改
+  mutations: {},
+  // 数据计算属性
+  getters(){
+
+  },
+  // 异步更改数据
+  actions: {},
+  // 模块化
+  modules: {}
+});
